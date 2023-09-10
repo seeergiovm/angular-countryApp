c9bb06b02a4e63a7f9ddd0dce8effadc30b9496a
Sergio Vargas <sergiovargas_1@outlook.com>
2023-09-10 23:07:42 +0200

    Aplicación funcional

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPageComponent {
+
+  public countries: Country[] = []
+
+  constructor( private countriesService: CountriesService) {}
+
+
+  searchByCapital( term: string ) {
+    this.countriesService.searchCapital(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
+      
+    console.log('Desde ByCapitalPage');
+    console.log({ term })
+  }
+}
---
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-region-page',
+  templateUrl: './by-region-page.component.html',
+  styles: [
+  ]
+})
+export class ByRegionPageComponent {
+
+  public countries: Country[] = [];
+
+
+  constructor(private countriesService: CountriesService) {}
+
+  searchByRegion(term: string) {
+    this.countriesService.searchRegion(term)
+      .subscribe(countries => this.countries = countries)
+  }
+
+}
